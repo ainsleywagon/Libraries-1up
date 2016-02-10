3983c79a92d6a6d8a92b4ce470f1ed7c2838f014
ainsleywagon <ainsley.wagoner@gmail.com>
2016-02-10 00:43:47 -0800

    Adding ScrollMagic library for fixing the sidenav.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-var $ = require('jquery');
-global.jQuery = global.$ = $;
-
+//ScrollIt.js function
 $(function(){
-  $.scrollIt();
+$.scrollIt();
 });
+
+var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+  duration: 2500, // the scene should last for a scroll distance of 2500px
+  offset: 300 // start this scene after scrolling 200px
+})
+.setPin('.sidenav')
+.addTo(controller);
