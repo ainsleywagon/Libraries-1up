dcbe5acee2d18fbb27a0b2b0064aec4b6912733b
ainsleywagon <ainsley.wagoner@gmail.com>
2016-02-10 18:02:15 -0800

    Changing dimensions of renditions and adding colors to swatch section.

--- js/app.js
+++ js/app.js
@@ -3,11 +3,18 @@ $(function(){
 $.scrollIt();
 });
 
+//Fixing the sidenav to a certain height
 var controller = new ScrollMagic.Controller();
 
 new ScrollMagic.Scene({
-  duration: 2500, // the scene should last for a scroll distance of 2500px
-  offset: 300 // start this scene after scrolling 200px
+  duration: 5000, // the scene should last for a scroll distance of 2500px
+  offset: 390 // start this scene after scrolling 200px
 })
 .setPin('.sidenav')
 .addTo(controller);
+
+// //Adding the current class onto the selected menu
+// $('.sidenav li a').click(function() {
+// $('.sidenav li a').removeClass(“current”);
+// $(this).parent().addClass(“current”);
+// });
